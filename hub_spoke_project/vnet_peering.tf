af14710e223ff2b0ebbedb012f5ce7ea30c6a2b2
Murugan <thangadurai.murugan@tesco.com>
2023-09-07 17:52:12 +0530

    vnet peering changed

--- hub_spoke_project/vnet_peering.tf
+++ hub_spoke_project/vnet_peering.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network_peering" "hub-spoke-nonprod-peer" {
   name                         = "hub-spoke-nonprod-peer"
   resource_group_name          = azurerm_resource_group.hubgroup.name
   virtual_network_name         = azurerm_virtual_network.hub.name
-  remote_virtual_network_id    = azurerm_virtual_network.hub.id
+  remote_virtual_network_id    = azurerm_virtual_network.nonprod.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network_peering" "nonprod-hub-spoke-peer" {
   name                         = "nonprod-hub-spoke-peer"
   resource_group_name          = azurerm_resource_group.nonprod.name
   virtual_network_name         = azurerm_virtual_network.nonprod.name
-  remote_virtual_network_id    = azurerm_virtual_network.nonprod.id
+  remote_virtual_network_id    = azurerm_virtual_network.hub.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true
@@ -29,7 +29,7 @@ resource "azurerm_virtual_network_peering" "hub-spoke-prod-peer" {
   name                         = "hub-spoke-prod-peer"
   resource_group_name          = azurerm_resource_group.hubgroup.name
   virtual_network_name         = azurerm_virtual_network.hub.name
-  remote_virtual_network_id    = azurerm_virtual_network.hub.id
+  remote_virtual_network_id    = azurerm_virtual_network.prod.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true
@@ -41,7 +41,7 @@ resource "azurerm_virtual_network_peering" "prod-hub-spoke-peer" {
   name                         = "prod-hub-spoke-peer"
   resource_group_name          = azurerm_resource_group.prod.name
   virtual_network_name         = azurerm_virtual_network.prod.name
-  remote_virtual_network_id    = azurerm_virtual_network.prod.id
+  remote_virtual_network_id    = azurerm_virtual_network.hub.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true
