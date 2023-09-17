c051bcb26e822602d48e7ba890076125e6b5ee70
Murugan <thangadurai.murugan@tesco.com>
2023-09-17 12:43:52 +0530

    slight changes in local variables

--- azure_vnet_subnets/locals.tf
+++ azure_vnet_subnets/locals.tf
@@ -1,3 +1,12 @@
+### local variables for Resource group section 
+locals {
+  rgp = {
+    groupname = "BlockstoneRGP"
+    location  = "useast"
+  }
+}
+
+### local variables for Tagging section for any resources 
 locals {
   default = {
     Environment     = "Development"
@@ -11,8 +20,20 @@ locals {
   }
 }
 
+### local variables for Subnet names
+locals {
+  subnet_names = {
+    privatesubnet1 = "Private-subnetA"
+    privatesubnet2 = "Private-subnetB"
+    publicsubnet1  = "Public-subnetA"
+    publicsubnet2  = "Public-subnetB"
+  }
+}
+
+### local variables for vnet,subnet ip ranges
 locals {
   vnetip          = "192.168.0.0/16"
+  vnetname        = "Blackstone-Network"
   private_subneta = "192.168.1.0/24"
   private_subnetb = "192.168.2.0/24"
   public_subneta  = "192.168.3.0/24"
--- azure_vnet_subnets/main.tf
+++ azure_vnet_subnets/main.tf
@@ -1,39 +1,39 @@
 resource "azurerm_resource_group" "bsgroup" {
-  name     = "BlockstoneRGP"
-  location = "useast"
+  name     = local.rgp.groupname
+  location = local.rgp.location
 }
 
 resource "azurerm_virtual_network" "blackstone" {
-  name                = "Blackstone-Network"
+  name                = local.vnetname
   resource_group_name = azurerm_resource_group.bsgroup.name
   location            = azurerm_resource_group.bsgroup.location
   address_space       = [local.vnetip]
-  tags                = merge(local.default, { Name = "Blackstone-Network" })
+  tags                = merge(local.default, { Name = local.vnetname })
 }
 
 resource "azurerm_subnet" "vlana" {
-  name                 = "Private-subnetA"
+  name                 = local.subnet_names.privatesubnet1
   resource_group_name  = azurerm_resource_group.bsgroup.name
   virtual_network_name = azurerm_virtual_network.blackstone.name
   address_prefixes     = [local.private_subneta]
   depends_on           = [azurerm_virtual_network.blackstone]
 }
 resource "azurerm_subnet" "vlanb" {
-  name                 = "Private-subnetB"
+  name                 = local.subnet_names.privatesubnet2
   resource_group_name  = azurerm_resource_group.bsgroup.name
   virtual_network_name = azurerm_virtual_network.blackstone.name
   address_prefixes     = [local.private_subnetb]
   depends_on           = [azurerm_virtual_network.blackstone]
 }
 resource "azurerm_subnet" "vlanc" {
-  name                 = "Public-subnetA"
+  name                 = local.subnet_names.publicsubnet1
   resource_group_name  = azurerm_resource_group.bsgroup.name
   virtual_network_name = azurerm_virtual_network.blackstone.name
   address_prefixes     = [local.public_subneta]
   depends_on           = [azurerm_virtual_network.blackstone]
 }
 resource "azurerm_subnet" "vland" {
-  name                 = "Public-subnetB"
+  name                 = local.subnet_names.publicsubnet2
   resource_group_name  = azurerm_resource_group.bsgroup.name
   virtual_network_name = azurerm_virtual_network.blackstone.name
   address_prefixes     = [local.public_subnetb]
