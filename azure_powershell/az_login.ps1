7b024a27619ff4cb49a167f36bfeb76778f4e0c4
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-02-11 22:35:42 +0530

    Update az_login.ps1

--- azure_powershell/az_login.ps1
+++ azure_powershell/az_login.ps1
@@ -5,10 +5,10 @@
 # Environment: Sandbox                                #
 # Contact: thangadurai.murugan@example.com            #
 #######################################################
-
 Write-Host "Script to login Azure account using PowerShell"  -BackgroundColor Black -ForegroundColor Red
-$tenant_id = ""
-$login=az login --tenant $tenant_id
+$tenant_id = Read-Host "Please enter the Tenant_Id:"
+Write-Host "The entered Tenant_Id is:" -BackgroundColor Yellow -ForegroundColor Red $tenant_id
+$login= az login --tenant $tenant_id
 Write-Output $login
 Start-Sleep 2
 Write-Host "Azure login is succeed" -BackgroundColor Black -ForegroundColor Red
