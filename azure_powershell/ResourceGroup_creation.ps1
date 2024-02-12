3968f24eb0f60052dbc0e7c317342be7a1d37d97
send2durai <send2durai@hotmail.com>
2024-02-12 08:43:45 +0530

    slight change

--- azure_powershell/ResourceGroup_creation.ps1
+++ azure_powershell/ResourceGroup_creation.ps1
@@ -6,8 +6,10 @@
 # Contact: thangadurai.murugan@example.com            #
 #######################################################
 Write-Host -ForegroundColor red -BackgroundColor Yellow "Script to create resource group in Azure Cloud"
-$rgp = Read-Host "Please enter the resource group that you wanted to create:"
-Write-Host "The entered ResoruceGroup name is:" -BackgroundColor Black -ForegroundColor Red $rgp
+$rgp_name = Read-Host "Please enter the resource group that you wanted to create:"
+Write-Host "The entered ResoruceGroup name is:" -BackgroundColor Black -ForegroundColor Red $rgp_name
 $place = Read-Host "Please enter the location where you need to avail this resource:"
 Write-Host "The entered location name is:" -BackgroundColor Black -ForegroundColor Red $place
-New-AzResourceGroup -Name $rgp -Location $place
+$rgp = New-AzResourceGroup -Name $rgp_name -Location $place
+write-output $rgp
+write-host "ResourceGroup has been created successfully" -BackgroundColor Black -ForegroundColor Red
